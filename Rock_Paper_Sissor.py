476f998d3d50a773ea825c2c21bbbaf75dad2c65
NewRepo <anikate955@gmail.com>
2025-06-28 14:33:20 +0530

    Rewrirte program

--- Rock_Paper_Sissor.py
+++ Rock_Paper_Sissor.py
@@ -3,26 +3,24 @@
 0 for paper 
 -1 for sissor
 '''
-import random
-computer= random.choice([1,0,-1])
-
+print("Let's Play ROCK / PAPER / SISSORS")
 print("Game Rules: \nr : Rock, p : paper, s : sessiors")
+import random
+computer=random.choice ([1,0,-1]) #1
+user=input("Enter your Choice: ") #r
 
-user=input("Enter your choice: ")
-dict= {"s": -1, "r": 1, "p":0}
+dict={"r":1, "p":0, "s":-1}
 if user not in dict:
-    print("You are enterd a wrong Entry..")
+    print("You are entry the wrong data..")
     exit()
+me=dict[user]
 
-me=(dict[user])
-
-new_dict= {-1: "Sissor", 0:"Paper", 1: "Rock"}
-print(f"You Chosse {new_dict[me]}\nComputer Chosse {new_dict[computer]}")
+new_dict={1:"rock", 0:"paper", -1:"sissor"}
+print(f"You Choose {new_dict[me]}\nComputer Choose {new_dict[computer]}")
 
 if (computer==me):
     print("It's a draw!")
 else:
-
     if(computer==1 and me==0):
         print("You Win!")
     elif(computer==1 and me==-1):
@@ -37,5 +35,3 @@ else:
         print("You Win!")
     else:
         print("Something Went Wrong, Please Try again.")
-
-
